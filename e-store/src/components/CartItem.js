cfca226e9cbc025d1b3e18a38585e13b2fbb0d5e
nknathi <nknathindamase@gmail.com>
2023-09-25 11:10:14 +0200

    CartItem component comments

--- e-store/src/components/CartItem.js
+++ e-store/src/components/CartItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+// defines a functional component named CartItem. 
+// It accepts a single argument props, which is an object containing properties passed to the component.
 const CartItem = props => {
+    // the cartItem and cartKey properties are extracted from the props object. 
+    // These properties are expected to be passed when using the CartItem component
     const { cartItem, cartKey } = props;
 
+    // destructures the cartItem object to extract the product and amount properties.
+    // It assumes that cartItem is an object with these properties
     const { product, amount } = cartItem;
 
+    // defines the JSX that will be rendered when the CartItem component is used.
+    // It creates a structured layout for displaying information about the product in the cart, including its name, price, description, and the quantity in the cart. 
+    // Additionally, it provides a delete button that invokes the removeFromCart function when clicked, passing cartKey as an argument.
     return (
         <div className=" column is-half">
             <div className="box">
@@ -31,4 +40,5 @@ const CartItem = props => {
     );
 };
 
+// CartItem component is exported so that it can be imported and used in other parts of your application.
 export default CartItem;
