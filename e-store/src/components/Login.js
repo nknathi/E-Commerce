59269fd387a95204defc7616c7bb2dfa40d95555
nknathi <nknathindamase@gmail.com>
2023-09-25 11:25:09 +0200

    Login component comments

--- e-store/src/components/Login.js
+++ e-store/src/components/Login.js
@@ -1,34 +1,54 @@
+//  imports the Component class
 import React, { Component } from "react";
+// used for programmatic navigation in the application.
 import { Navigate } from "react-router-dom";
+// higher-order component (HOC) that wraps the Login component with some additional context-related functionality
 import withContext from "../withContext";
 
+// class named Login is declared, and it extends the Component class from React
+// means Login is a React class component.
 class Login extends Component {
     constructor(props) {
       super(props);
+
+      // component initializes its state with two properties:
+      // username and password, both initially set to empty strings.
       this.state = {
         username: "",
         password: ""
       };
     }
   
+    // method that is used to update the state when input fields change.
+    // It uses the e.target.name to dynamically update the corresponding state property with the new value entered by the user.
     handleChange = e => this.setState({ [e.target.name]: e.target.value, error: "" });
   
+    // method that handles the form submission 
     login = (e) => {
+      // prevents the default form submission behavior (page refresh)
       e.preventDefault();
   
+      // extracts the username and password from the component's state.
       const { username, password } = this.state;
+
+      // checks if both fields are filled; if not, it sets an error message in the state
       if (!username || !password) {
         return this.setState({ error: "Fill all fields!" });
       }
+      // If both fields are filled, it calls a login function provided via props from the context 
       this.props.context.login(username, password)
-        .then((loggedIn) => {
-          if (!loggedIn) {
+        .then((loggedIn) => { // expects the login function to return a Promise that resolves to a boolean value indicating whether the login was successful or not.
+          if (!loggedIn) { // unsuccessful (the Promise resolves to false), it sets an "Invalid Credentials" error message in the state.
             this.setState({ error: "Invalid Credentails" });
           }
         })
     };
   
+    //  render method contains a conditional statement that checks the value of this.props.context.user.
+    // Depending on whether user is falsy (likely indicating that the user is not logged in) or truthy (indicating that the user is logged in),
+    // different JSX elements are returned.
     render() {
+      // If this.props.context.user is falsy (not logged in), the following JSX is returned:
       return !this.props.context.user ? (
         <>
           <div className="hero is-primary ">
@@ -72,6 +92,8 @@ class Login extends Component {
           </form>
         </>
       ) : (
+        // If this.props.context.user is truthy (logged in), it renders a <Navigate to="/products" /> component. 
+        // This suggests that the user is redirected to a "/products" route upon successful login.
         <Navigate to="/products" />
       );
     }
